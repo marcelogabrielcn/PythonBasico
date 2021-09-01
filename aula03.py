b9835849e316c3223dd167534c895f04bfba5f8f
Marcelo Gabriel <marcelogabrielcn@outlook.com>
2021-08-31 21:37:20 -0300

    Strings

---
+++ aula03.py
@@ -0,0 +1,22 @@
+# Vamos começar estudar sobre Strings (str)
+
+print('Essa é uma String (Texto)')
+# print('Aqui vai dar erro com meu 'texto' porque ele fecha na segunda aspa')
+print('Aqui podemos usar "aspas" dentro da String')
+
+# Em Python, as Strings podem ser escritas com aspas simples ou aspas duplas.
+# Portanto para usar aspas dentro de um texto, basta usar um tipo diferente de aspas
+
+# Podemos ainda usar "\" para ignorar algo dentro da String.
+# Isso se chama cractere de escape.
+
+print('Esse é meu \'texto\' (str)')
+
+# Podemos inserir uma quebra de linha dentro de uma String.
+# Basta usarmos \n.
+
+print('Esse texto ficará quebrado em: \nlinha 1 \nlinha 2 \nlinha 3')
+
+# Caso queira ignorar a quebra de linha e usar o \n podemos fazer assim:
+
+print(r'Esse texto não ficará quebrado nem com o \n porque eu ignorei com o r no início.')
