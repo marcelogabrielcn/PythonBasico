2159e49fa24b0017c69ba4676fa05d3d7ba6af65
Marcelo Gabriel <marcelogabrielcn@outlook.com>
2021-09-02 21:53:46 -0300

    Cadastro Simples

---
+++ aula04exe01.py
@@ -0,0 +1,15 @@
+"""
+Criar uma espécie de tela de cadastro simples. Com nome, idade, altura e verificar se é maior de idade.
+"""
+
+# Nome: String
+print('Marcelo', type('Marcelo'))
+
+# Idade: int
+print(22, type(22))
+
+# Altura: float
+print(1.86, type(1.86))
+
+# É maior?: bool
+print(22 >= 18, type(22 >= 18))
