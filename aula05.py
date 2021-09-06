54133b922c30d74b6afe38bf4452ee13ef18585f
Marcelo Gabriel <marcelogabrielcn@outlook.com>
2021-09-06 09:46:53 -0300

    Operadores Aritméticos
    Conhecendo os operadores aritméticos e ordem de precedência

---
+++ aula05.py
@@ -0,0 +1,29 @@
+"""
+Operadores Aritméticos
+
++  Soma
+-  Subtração
+*  Multiplicação
+/  Divisão
+// Divisão inteiro
+** Exponenciação
+%  Resto da divisão
+() Ordem de Precedência
+"""
+
+print(10 + 10)
+print(10 - 10)
+print(10 * 10)
+print(10 / 10)
+print(10 // 3)
+print(10 ** 2)
+print(10 % 3)
+
+# O operador de multiplicação, também serve como repetição
+print('K' * 30)
+
+# O operador de soma, pode ser usado para fazer concatenação de Strings
+print('Olá' + ' Mundo!')
+
+# A ordem de precedência é igual na matemática, primeiro os parênteses, potenciação...
+# ...multiplicação, divisão, soma e subtração
