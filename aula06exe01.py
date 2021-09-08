56a89a2a7418cf1f00cf6f98faae4971fb817d6a
Marcelo Gabriel <marcelogabrielcn@outlook.com>
2021-09-07 21:05:48 -0300

    Calculadora IMC
    Exercício simples de calculo de IMC, apenas para exercitar o uso das variáveis.

---
+++ aula06exe01.py
@@ -0,0 +1,13 @@
+"""
+Calculo de IMC
+Criar um print com informações de variáveis e um calculo de IMC
+"""
+
+nome = 'Marcelo Gabriel'
+idade = 22
+peso = 72.8
+altura = 1.86
+
+imc = peso / (altura ** 2)
+
+print(nome, 'tem', idade, 'anos de idade e seu IMC é: ', imc)
