1aa2ece17b8b9ffd0aa725d9d7c5c7139d529063
Marcelo Gabriel <marcelogabrielcn@outlook.com>
2021-09-07 21:04:56 -0300

    Variáveis
    Aula sobre uso das variáveis

---
+++ aula06.py
@@ -0,0 +1,22 @@
+"""
+Variáveis
+São itens que podemos armazenar na memória do computador
+Deve começar com letras e pode conter números, _ letras maiúsculas.
+"""
+
+nome = 'Marcelo Gabriel'
+print(nome)
+
+# Criada a variável "nome" eu armazeno um valor nela, nesse caso do tipo String
+
+idade = 22
+print(nome, 'tem', idade, 'anos.')
+
+# Posso usar variáveis com nomes compostos
+ano_atual = 2021
+
+# Posso fazer contas com variáveis.
+nota1 = 10
+nota2 = 7.5
+media = (nota1 + nota2) / 2
+print(media)
