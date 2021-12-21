7938c60f2417f9b826d424d6d056abe716cbf362
Marcelo Gabriel <marcelogabrielcn@gmail.com>
2021-12-20 21:14:29 -0300

    While - Estrutura de repetição em Python
    A estrutura While serve para criar um laço de repetição. While em inglês significa enquanto. Portanto, a estrutura quer dizer, enquanto tal condição for verdadeira, faça tal coisa.

---
+++ 34.While.py
@@ -0,0 +1,14 @@
+"""
+A estrutura While serve para criar um laço de repetição.
+While em inglês significa enquanto. Portanto, a estrutura quer dizer,
+enquanto tal condição for verdadeira, faça tal coisa.
+"""
+
+x = 0
+while x < 6:
+    y = 0
+    while y < 6:
+        print(f'({x}, {y})')
+        y += 1
+    x += 1
+print('fim')
