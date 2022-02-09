c47299e880b4afab6e4430d3d7e72be9d9bd7b37
Marcelo Gabriel <marcelogabrielcn@gmail.com>
2022-02-09 18:45:34 -0300

    Add files via upload

---
+++ 47. Desafio de contadores.py
@@ -0,0 +1,19 @@
+"""
+Desafio para testar conhecimentos até aqui
+"""
+
+c2 = 10
+c1 = 0
+
+for n in range(9):
+    print(c1, c2)
+    c2 -= 1
+    c1 += 1
+
+c3 = 10
+c4 = 0
+
+while c3 > 1 and c4 < 9:
+    print(c4, c3)
+    c3 -= 1
+    c4 += 1
